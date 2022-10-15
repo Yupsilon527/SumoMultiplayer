d41ae0580b071775b07fee6f0cc1914c7ee259e2
eespee <paulsendroiu@gmail.com>
2022-10-15 16:40:06 +0300

    Everything

--- Assets/Script/UI/MuteButton.cs
+++ Assets/Script/UI/MuteButton.cs
@@ -6,6 +6,7 @@ using UnityEngine.Audio;
 public class MuteButton : MonoBehaviour, iButton
 {
     public AudioMixer controlled;
+    public GameObject DisabledSprite;
     public string ParameterName;
 
     float ParameterDefaultValue;
@@ -37,7 +38,8 @@ public class MuteButton : MonoBehaviour, iButton
     {
         muted = value;
         PlayerPrefs.SetInt(ParameterName + "_muted", muted ? 1 : 0);
-        controlled.SetFloat(ParameterName, muted ? 0 : ParameterDefaultValue);
+        controlled.SetFloat(ParameterName, muted ? -80 : ParameterDefaultValue);
+        DisabledSprite.SetActive(muted);
     }
 
     private void OnDisable()
